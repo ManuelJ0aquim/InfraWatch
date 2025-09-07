4790edc714fe032b63bfccf44c8790682362a806
Marques_Sanches <marquessanches007@gmail.com>
2025-09-07 10:02:58 +0100

    update

--- dist/Monitoring/Queue/redis.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redisConnection = void 0;
-exports.redisConnection = {
-    url: process.env.REDIS_URL || 'redis://127.0.0.1:6379',
-};
